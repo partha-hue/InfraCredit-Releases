6e1bcaef448e58d1c2a7b180e563970058b5e751
Partha <parthachakraborty383@gmail.com>
2026-01-28 02:01:53 +0530

    Production Fix: Hardened local database initialization and backup ZIP strategy. Release v1.20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.example.infracredit"
         minSdk = 24
         targetSdk = 35
-        versionCode = 19
-        versionName = "1.19"
+        versionCode = 20
+        versionName = "1.20"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
