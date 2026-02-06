7506c079e67d44eb670d017a0ac0d99a1475a965
Partha <parthachakraborty383@gmail.com>
2026-02-06 15:35:21 +0530

    Production v1.29: Language support and Auto-SMS fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.example.infracredit"
         minSdk = 24
         targetSdk = 35
-        versionCode = 28
-        versionName = "1.28"
+        versionCode = 29
+        versionName = "1.29"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
